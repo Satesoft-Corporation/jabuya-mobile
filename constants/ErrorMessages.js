6afb35784b851d9e07553c3b76c9224307b79de3
Ssembatya Moses <104248822+Mosesjespar@users.noreply.github.com>
2023-09-16 10:07:47 +0300

    Create ErrorMessages.js

---
+++ constants/ErrorMessages.js
@@ -0,0 +1,4 @@
+export const INTERNAL_SERVER_ERROR = 'Something went wrong. Please try again or contact support';
+export const DEFAULT_ERROR_MESSAGE = {'status': 'FAILED', 'responseMessage': INTERNAL_SERVER_ERROR};
+export const NO_RECORDS_FOUND_ERROR_MESSAGE = 'No records were found.';
+export const ACCOUNT_ID_NOT_SET_ERROR_MESSAGE = 'Opportunity account id is not set';
