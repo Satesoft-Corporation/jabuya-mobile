ba93cdcdda86656e46b082972878408a8dfed484
Ssembatya Moses <104248822+Mosesjespar@users.noreply.github.com>
2023-09-26 22:53:27 +0300

    updating dialog UI

--- components/ConfirmSalesDialog.js
+++ components/ConfirmSalesDialog.js
@@ -1,97 +1,136 @@
-import { Text, View, FlatList, Image } from "react-native";
+import { Text, View, FlatList, Image, ScrollView } from "react-native";
 import React from "react";
 import ModalContent from "./ModalContent";
 import Card from "./Card";
 import Colors from "../constants/Colors";
 import MaterialButton from "./MaterialButton";
+import { Table, Row, Rows, TableWrapper } from "react-native-table-component";
 
-function ConfirmSalesDialog({ visible, navigation, addSale }) {
+function ConfirmSalesDialog({ visible, navigation, addSale, sales, total }) {
+  const tableHead = ["Product", "Qnty", "Amount"];
+
   return (
-    <ModalContent visible={visible} style={{ padding: 35 }}>
-      <Card style={{ alignSelf: "center", minHeight: 120 }}>
-        <View
-          style={{
-            backgroundColor: Colors.light,
-            padding: 2,
-          }}
-        >
+    <ModalContent visible={visible} style={{ padding: 30 }}>
+      <Card style={{ alignSelf: "center", minHeight: 120, maxHeight: 400 }}>
+        <ScrollView showsHorizontalScrollIndicator={false}>
           <View
             style={{
-              flexDirection: "row",
-              alignItems: "center",
+              backgroundColor: Colors.light,
+              padding: 2,
             }}
           >
-            <Text
+            <View
               style={{
-                marginTop: 10,
-                fontWeight: "bold",
-                fontSize: 18,
-                marginBottom: 5,
-                marginStart: 10,
+                flexDirection: "row",
+                alignItems: "center",
               }}
             >
-              Sale Confirmed
-            </Text>
-            <Image
-              source={require("../assets/icons/green_tick.png")}
+              <Text
+                style={{
+                  marginTop: 10,
+                  fontWeight: "bold",
+                  fontSize: 18,
+                  marginBottom: 5,
+                  marginStart: 10,
+                }}
+              >
+                Sale Confirmed
+              </Text>
+              <Image
+                source={require("../assets/icons/green_tick.png")}
+                style={{
+                  height: 30,
+                  width: 30,
+                  resizeMode: "contain",
+                  marginStart: 15,
+                  alignSelf: "center",
+                }}
+              />
+            </View>
+
+            <Table>
+              <Row
+                data={tableHead}
+                style={{ height: 40 }}
+                textStyle={{
+                  fontWeight: "bold",
+                }}
+                flexArr={[2, 1, 1]}
+              />
+
+              <TableWrapper style={{ flexDirection: "row" }}>
+                <Rows
+                  data={sales}
+                  textStyle={{
+                    margin: 5,
+                    textAlign: "left",
+                  }}
+                  flexArr={[2, 1, 1]}
+                />
+              </TableWrapper>
+              <View
+                style={{
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                }}
+              >
+                <Text style={{ fontWeight: "bold" }}>Total Cost</Text>
+                <Text
+                  style={{
+                    alignSelf: "flex-end",
+                    fontWeight: "bold",
+                    marginEnd: 4,
+                  }}
+                >
+                  UGX {total}
+                </Text>
+              </View>
+            </Table>
+
+            <View
               style={{
-                height: 30,
-                width: 30,
-                resizeMode: "contain",
-                marginStart: 15,
+                flexDirection: "row",
+                justifyContent: "space-between",
                 alignSelf: "center",
               }}
-            />
+            >
+              <MaterialButton
+                title="Add Sale"
+                style={{
+                  backgroundColor: Colors.dark,
+                  borderRadius: 5,
+                  borderWidth: 1,
+                  borderColor: Colors.primary,
+                  width: 100,
+                  margin: 10,
+                  height: 40,
+                }}
+                titleStyle={{
+                  fontWeight: "bold",
+                  color: Colors.primary,
+                }}
+                buttonPress={() => addSale()}
+              />
+              <MaterialButton
+                title="View Sales"
+                style={{
+                  backgroundColor: Colors.dark,
+                  borderRadius: 5,
+                  borderWidth: 1,
+                  borderColor: Colors.primary,
+                  width: 100,
+                  margin: 10,
+                  height: 40,
+                }}
+                titleStyle={{
+                  fontWeight: "bold",
+                  color: Colors.primary,
+                }}
+                buttonPress={() => addSale()}
+              />
+            </View>
           </View>
-
-          <View
-            style={{
-              flexDirection: "row",
-              justifyContent: "space-between",
-              // alignItems:'center',
-              alignSelf: "center",
-              margin: 10,
-            }}
-          >
-            <MaterialButton
-              title="Add Sale"
-              style={{
-                backgroundColor: Colors.dark,
-                borderRadius: 5,
-                borderWidth: 1,
-                borderColor: Colors.primary,
-                width: 100,
-                margin: 10,
-                height: 40,
-              }}
-              titleStyle={{
-                fontWeight: "bold",
-                color: Colors.primary,
-              }}
-              buttonPress={() => addSale()}
-            />
-            <MaterialButton
-              title="View Sales"
-              style={{
-                backgroundColor: Colors.dark,
-                borderRadius: 5,
-                borderWidth: 1,
-                borderColor: Colors.primary,
-                width: 100,
-                margin: 10,
-                height: 40,
-              }}
-              titleStyle={{
-                fontWeight: "bold",
-                color: Colors.primary,
-              }}
-              buttonPress={() => {
-                navigation.navigate("viewSales");
-                addSale();
-              }}
-            />
-          </View>
-        </View>
+        </ScrollView>
       </Card>
     </ModalContent>
   );
