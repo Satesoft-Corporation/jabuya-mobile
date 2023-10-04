76674eb225b5f6d43279184d3ce8e432cf315e8a
Ssembatya Moses <104248822+Mosesjespar@users.noreply.github.com>
2023-10-04 22:05:08 +0300

    Update ViewSales.js

--- screens/ViewSales.js
+++ screens/ViewSales.js
@@ -14,13 +14,9 @@ import AppStatusBar from "../components/AppStatusBar";
 import MultiColumnView from "../components/MultiColumnView";
 import HeaderOneButton from "../components/HeaderOneButton";
 import OrientationLoadingOverlay from "react-native-orientation-loading-overlay";
-import { Table, Row, Rows, TableWrapper } from "react-native-table-component";
-import ModalContent from "../components/ModalContent";
-import Card from "../components/Card";
-import ButtonClose from "../components/ButtonClose";
+import SaleSummaryDialog from "../components/SaleSummaryDialog";
 
 const screenWidth = Dimensions.get("window").width;
-const tableHead = ["Product", "Unit Price", "Qnty", "Amount"];
 
 export default function ViewSales({ navigation }) {
   const [sales, setSales] = useState([]); // sales got from the server
@@ -111,7 +107,7 @@ export default function ViewSales({ navigation }) {
         setTotalSales(response.totalItems);
         setTimeout(() => {
           setLoading(false);
-        }, 1000);
+        }, 100);
       })
       .catch((error) => {
         Alert.alert("Cannot get sales!", error?.message);
@@ -123,7 +119,6 @@ export default function ViewSales({ navigation }) {
     getSales();
   }, []);
 
-  
   const showSummary = (item) => {
     setLoading(true);
     if (item.lineItems !== undefined) {
@@ -205,92 +200,12 @@ export default function ViewSales({ navigation }) {
             />
           </View>
         </View>
-        <ModalContent
+        <SaleSummaryDialog
           visible={visible}
-          onBackDropPress={() => closeSummary()}
-          style={{ padding: 15 }}
-        >
-          <Card style={{ maxHeight: 400, paddingBottom: 20, minHeight: 120 }}>
-            <View style={{ height: 40 }}>
-              <ButtonClose onClose={() => closeSummary()} />
-            </View>
-            <Text
-              style={{
-                fontSize: 18,
-                fontWeight: 500,
-                marginBottom: 5,
-                marginStart: 10,
-              }}
-            >
-              Sale Summary
-            </Text>
-            <ScrollView showsHorizontalScrollIndicator={false}>
-              <View style={{ flex: 1, paddingHorizontal: 10 }}>
-                <Table style={{ paddingBottom: 10 }}>
-                  <Row
-                    data={tableHead}
-                    style={{ height: 40 }}
-                    textStyle={{
-                      fontWeight: 600,
-                    }}
-                    flexArr={[2.5, 1, 0.8, 0.8]}
-                  />
-
-                  <TableWrapper style={{ flexDirection: "row" }}>
-                    <Rows
-                      style={{
-                        borderTopColor: Colors.dark,
-                        borderTopWidth: 0.5,
-                      }}
-                      data={lineItems}
-                      textStyle={{
-                        margin: 5,
-                        textAlign: "left",
-                      }}
-                      flexArr={[2.5, 1, 0.8, 0.8]}
-                    />
-                  </TableWrapper>
-                </Table>
-                <View
-                  style={{
-                    backgroundColor: Colors.light,
-                    borderRadius: 5,
-                    padding: 10,
-                    flexDirection: "row",
-                    justifyContent: "space-between",
-                  }}
-                >
-                  <View
-                    style={{
-                      flexDirection: "row",
-                      alignItems: "center",
-                    }}
-                  >
-                    <View>
-                      <Text style={{ fontWeight: "bold" }}>
-                        Recieved Amount
-                      </Text>
-                    </View>
-                  </View>
-                  <View
-                    style={{
-                      justifyContent: "center",
-                      alignItems: "flex-end",
-                    }}
-                  >
-                    <Text
-                      style={{
-                        fontWeight: 500,
-                      }}
-                    >
-                      UGX {sale && sale.totalCost}
-                    </Text>
-                  </View>
-                </View>
-              </View>
-            </ScrollView>
-          </Card>
-        </ModalContent>
+          closeSummary={closeSummary}
+          sale={sale}
+          lineItems={lineItems}
+        />
       </ScrollView>
     </SafeAreaView>
   );
