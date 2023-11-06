8cde35e25cc5b2e0a56a609d8c8a5354fb553c30
Ssembatya Moses <104248822+Mosesjespar@users.noreply.github.com>
2023-11-06 14:25:35 +0300

    componet UI change

--- components/ConfirmSalesDialog.js
+++ components/ConfirmSalesDialog.js
@@ -1,4 +1,4 @@
-import { Text, View, FlatList, Image, ScrollView } from "react-native";
+import { Text, View, TouchableOpacity, Image, ScrollView } from "react-native";
 import React from "react";
 import ModalContent from "./ModalContent";
 import Card from "./Card";
@@ -6,12 +6,29 @@ import Colors from "../constants/Colors";
 import MaterialButton from "./MaterialButton";
 import { Table, Row, Rows, TableWrapper } from "react-native-table-component";
 
-function ConfirmSalesDialog({ visible, navigation, addSale, sales, total,setVisible,clear }) {
-  const tableHead = ["Product", "Qnty", "Amount"];
+function ConfirmSalesDialog({
+  visible,
+  addSale,
+  sales,
+  total,
+  setVisible,
+  transID,
+  length,
+  balanceGivenOut,
+  amountPaid,
+}) {
+  const tableHead = ["Item", "Qty", "Price", "Amount"];
 
   return (
-    <ModalContent visible={visible} style={{ padding: 30 }}>
-      <Card style={{ alignSelf: "center", minHeight: 120, maxHeight: 400 }}>
+    <ModalContent visible={visible} style={{ padding: 10 }}>
+      <Card
+        style={{
+          alignSelf: "center",
+          minHeight: 120,
+          maxHeight: 400,
+          width: 315,
+        }}
+      >
         <ScrollView showsHorizontalScrollIndicator={false}>
           <View
             style={{
@@ -22,7 +39,7 @@ function ConfirmSalesDialog({ visible, navigation, addSale, sales, total,setVisi
             <View
               style={{
                 flexDirection: "row",
-                alignItems: "center",
+                justifyContent: "space-between",
               }}
             >
               <Text
@@ -31,23 +48,30 @@ function ConfirmSalesDialog({ visible, navigation, addSale, sales, total,setVisi
                   fontWeight: "bold",
                   fontSize: 18,
                   marginBottom: 5,
-                  marginStart: 10,
+                  marginStart: 1,
                 }}
               >
-                Sale Confirmed
+                Confirm Sale
               </Text>
-              <Image
-                source={require("../assets/icons/green_tick.png")}
-                style={{
-                  height: 30,
-                  width: 30,
-                  resizeMode: "contain",
-                  marginStart: 15,
-                  alignSelf: "center",
-                }}
-              />
+              <TouchableOpacity>
+                <Image
+                  source={require("../assets/icons/ic_close.png")}
+                  style={{
+                    height: 12,
+                    width: 12,
+                    resizeMode: "contain",
+                    marginStart: 15,
+                    alignSelf: "center",
+                    marginBottom: 4,
+                  }}
+                />
+              </TouchableOpacity>
             </View>
 
+            <View style={{ flexDirection: "row" }}>
+              <Text>Txn ID :</Text>
+              <Text> {transID}</Text>
+            </View>
             <Table>
               <Row
                 data={tableHead}
@@ -55,7 +79,7 @@ function ConfirmSalesDialog({ visible, navigation, addSale, sales, total,setVisi
                 textStyle={{
                   fontWeight: "bold",
                 }}
-                flexArr={[2, 1, 1]}
+                flexArr={[2, 1, 1, 1]}
               />
 
               <TableWrapper style={{ flexDirection: "row" }}>
@@ -65,16 +89,17 @@ function ConfirmSalesDialog({ visible, navigation, addSale, sales, total,setVisi
                     margin: 5,
                     textAlign: "left",
                   }}
-                  flexArr={[2, 1, 1]}
+                  flexArr={[2, 1, 1, 1]}
                 />
               </TableWrapper>
               <View
                 style={{
                   flexDirection: "row",
                   justifyContent: "space-between",
+                  marginBottom: 5,
                 }}
               >
-                <Text style={{ fontWeight: "bold" }}>Total Cost</Text>
+                <Text style={{ fontWeight: "bold" }}>Total </Text>
                 <Text
                   style={{
                     alignSelf: "flex-end",
@@ -82,7 +107,72 @@ function ConfirmSalesDialog({ visible, navigation, addSale, sales, total,setVisi
                     marginEnd: 4,
                   }}
                 >
-                  UGX {total}
+                  {length}
+                </Text>
+                <Text
+                  style={{
+                    alignSelf: "flex-end",
+                    fontWeight: "bold",
+                    marginEnd: 4,
+                  }}
+                >
+                  <Text style={{ fontSize: 11, color: Colors.gray }}>UGX</Text>{" "}
+                  {total}
+                </Text>
+              </View>
+              <View
+                style={{
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                }}
+              >
+                <Text style={{ fontWeight: "bold" }}>Recieved </Text>
+                <Text
+                  style={{
+                    alignSelf: "flex-end",
+                    fontWeight: "bold",
+                    marginEnd: 4,
+                  }}
+                >
+                  <Text style={{ fontSize: 11, color: Colors.gray }}>UGX</Text>{" "}
+                  {amountPaid}
+                </Text>
+              </View>
+              <View
+                style={{
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                }}
+              >
+                <Text style={{ fontWeight: "bold" }}>Purchased </Text>
+                <Text
+                  style={{
+                    alignSelf: "flex-end",
+                    fontWeight: "bold",
+                    marginEnd: 4,
+                  }}
+                >
+                  <Text style={{ fontSize: 11, color: Colors.gray }}>UGX</Text>{" "}
+                  {total}
+                </Text>
+              </View>
+
+              <View
+                style={{
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                }}
+              >
+                <Text style={{ fontWeight: "bold" }}>Balance</Text>
+                <Text
+                  style={{
+                    alignSelf: "flex-end",
+                    fontWeight: "bold",
+                    marginEnd: 4,
+                  }}
+                >
+                  <Text style={{ fontSize: 11, color: Colors.gray }}>UGX</Text>{" "}
+                  {balanceGivenOut}
                 </Text>
               </View>
             </Table>
@@ -91,17 +181,37 @@ function ConfirmSalesDialog({ visible, navigation, addSale, sales, total,setVisi
               style={{
                 flexDirection: "row",
                 justifyContent: "space-between",
-                alignSelf: "center",
+                marginTop: 10,
               }}
             >
               <MaterialButton
-                title="Add Sale"
+                title="Cancel"
+                style={{
+                  backgroundColor: "transparent",
+                  borderRadius: 5,
+                  borderWidth: 1,
+                  borderColor: Colors.dark,
+                  marginStart: -2,
+                  margin: 10,
+                  height: 40,
+                }}
+                titleStyle={{
+                  fontWeight: "bold",
+                  color: Colors.dark,
+                }}
+                buttonPress={() => {
+                  setVisible();
+                }}
+              />
+              <MaterialButton
+                title="OK"
                 style={{
                   backgroundColor: Colors.dark,
                   borderRadius: 5,
                   borderWidth: 1,
-                  borderColor: Colors.primary,
-                  width: 100,
+                  borderColor: Colors.dark,
+                  marginStart: 2,
+                  marginEnd: -2,
                   margin: 10,
                   height: 40,
                 }}
@@ -110,27 +220,6 @@ function ConfirmSalesDialog({ visible, navigation, addSale, sales, total,setVisi
                   color: Colors.primary,
                 }}
                 buttonPress={() => addSale()}
-              />
-              <MaterialButton
-                title="View Sales"
-                style={{
-                  backgroundColor: Colors.dark,
-                  borderRadius: 5,
-                  borderWidth: 1,
-                  borderColor: Colors.primary,
-                  width: 100,
-                  margin: 10,
-                  height: 40,
-                }}
-                titleStyle={{
-                  fontWeight: "bold",
-                  color: Colors.primary,
-                }}
-                buttonPress={() =>{
-                  setVisible()
-                  clear()
-                  navigation.navigate('viewSales')
-                }}
               />
             </View>
           </View>
