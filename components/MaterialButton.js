98f726e21eb63526a37eae321159747fb656df00
Ssembatya Moses <104248822+Mosesjespar@users.noreply.github.com>
2023-11-06 14:21:31 +0300

    removing shadows

--- components/MaterialButton.js
+++ components/MaterialButton.js
@@ -1,21 +1,38 @@
-import React from 'react';
+import React from "react";
 import { Text, TouchableOpacity } from "react-native";
 
 function MaterialButton({ buttonPress, title, style, titleStyle = {} }) {
-
-    return (
-        <TouchableOpacity style={[{
-            height: 44,
-            alignItems: 'center',
-            justifyContent: 'center',
-            elevation: 3,
-            shadowOffset: { width: 0, height: 2 },
-            shadowOpacity: 0.3
-        }, style]} onPress={buttonPress}>
-            <Text style={[{ fontSize: 14, fontWeight: "bold", color: (style.backgroundColor === 'white' || style.backgroundColor === 'transparent') ? 'black' : 'white' }, titleStyle]}>{title}</Text>
-        </TouchableOpacity>
-
-    );
+  return (
+    <TouchableOpacity
+      style={[
+        {
+          height: 44,
+          alignItems: "center",
+          justifyContent: "center",
+          flex:1
+        },
+        style,
+      ]}
+      onPress={buttonPress}
+    >
+      <Text
+        style={[
+          {
+            fontSize: 14,
+            fontWeight: "bold",
+            color:
+              style.backgroundColor === "white" ||
+              style.backgroundColor === "transparent"
+                ? "black"
+                : "white",
+          },
+          titleStyle,
+        ]}
+      >
+        {title}
+      </Text>
+    </TouchableOpacity>
+  );
 }
 
 export default MaterialButton;
