4ea3c3ef42419e431272f8ab21d12c18ed8610cf
Ssembatya Moses <104248822+Mosesjespar@users.noreply.github.com>
2023-11-07 17:52:56 +0300

    Update ConfirmSalesDialog.js

--- components/ConfirmSalesDialog.js
+++ components/ConfirmSalesDialog.js
@@ -16,7 +16,7 @@ function ConfirmSalesDialog({
   length,
   balanceGivenOut,
   amountPaid,
-  resetList
+  resetList,
 }) {
   const tableHead = ["Item", "Qty", "Price", "Amount"];
 
@@ -26,206 +26,215 @@ function ConfirmSalesDialog({
         style={{
           alignSelf: "center",
           minHeight: 120,
-          maxHeight: 400,
+          maxHeight: 490,
           width: 315,
+          paddingBottom:7
         }}
       >
-        <ScrollView showsHorizontalScrollIndicator={false}>
+        <View
+          style={{
+            backgroundColor: Colors.light,
+            padding: 2,
+          }}
+        >
           <View
             style={{
-              backgroundColor: Colors.light,
-              padding: 2,
+              flexDirection: "row",
+              justifyContent: "space-between",
             }}
           >
-            <View
+            <Text
               style={{
-                flexDirection: "row",
-                justifyContent: "space-between",
+                marginTop: 10,
+                fontWeight: "bold",
+                fontSize: 18,
+                marginBottom: 12,
+                marginStart: 1,
               }}
             >
-              <Text
+              Confirm sale
+            </Text>
+            <TouchableOpacity
+              onPress={() => {
+                setVisible();
+                resetList();
+              }}
+            >
+              <Image
+                source={require("../assets/icons/ic_close.png")}
                 style={{
+                  height: 12,
+                  width: 12,
+                  resizeMode: "contain",
+                  marginStart: 15,
+                  alignSelf: "center",
                   marginTop: 10,
-                  fontWeight: "bold",
-                  fontSize: 18,
-                  marginBottom: 5,
-                  marginStart: 1,
                 }}
-              >
-                Confirm Sale
-              </Text>
-              <TouchableOpacity onPress={()=>setVisible()}>
-                <Image
-                  source={require("../assets/icons/ic_close.png")}
-                  style={{
-                    height: 12,
-                    width: 12,
-                    resizeMode: "contain",
-                    marginStart: 15,
-                    alignSelf: "center",
-                    marginTop:10
-                  }}
-                />
-              </TouchableOpacity>
-            </View>
+              />
+            </TouchableOpacity>
+          </View>
 
-            <View style={{ flexDirection: "row" }}>
-              <Text>Txn ID :</Text>
-              <Text> {transID}</Text>
-            </View>
-            <Table>
-              <Row
-                data={tableHead}
-                style={{ height: 40 }}
+          <View
+            style={{ flexDirection: "row", justifyContent: "space-between" }}
+          >
+            <Text>
+              Txn ID : <Text> {transID}</Text>
+            </Text>
+
+            <Text>Currency : UGX</Text>
+          </View>
+
+          <Table>
+            <Row
+              data={tableHead}
+              style={{ height: 40 }}
+              textStyle={{
+                fontWeight: "bold",
+              }}
+              flexArr={[2, 1, 1, 1]}
+            />
+            <ScrollView style={{ height: 200 }}>
+              <Rows
+                data={sales}
                 textStyle={{
-                  fontWeight: "bold",
+                  margin: 5,
+                  textAlign: "left",
                 }}
                 flexArr={[2, 1, 1, 1]}
               />
+            </ScrollView>
 
-              <TableWrapper style={{ flexDirection: "row" }}>
-                <Rows
-                  data={sales}
-                  textStyle={{
-                    margin: 5,
-                    textAlign: "left",
-                  }}
-                  flexArr={[2, 1, 1, 1]}
-                />
-              </TableWrapper>
-              <View
+            <View
+              style={{
+                marginTop: 10,
+                flexDirection: "row",
+                justifyContent: "space-between",
+                marginBottom: 5,
+                fontSize: 15,
+              }}
+            >
+              <Text style={{ fontWeight: "bold" }}>Total </Text>
+              <Text
                 style={{
-                  flexDirection: "row",
-                  justifyContent: "space-between",
-                  marginBottom: 5,
+                  alignSelf: "flex-end",
+                  fontWeight: "bold",
+                  marginEnd: 4,
                 }}
               >
-                <Text style={{ fontWeight: "bold" }}>Total </Text>
-                <Text
-                  style={{
-                    alignSelf: "flex-end",
-                    fontWeight: "bold",
-                    marginEnd: 4,
-                  }}
-                >
-                  {length}
-                </Text>
-                <Text
-                  style={{
-                    alignSelf: "flex-end",
-                    fontWeight: "bold",
-                    marginEnd: 4,
-                  }}
-                >
-                  <Text style={{ fontSize: 11, color: Colors.gray }}>UGX</Text>{" "}
-                  {total}
-                </Text>
-              </View>
-              <View
+                {length}
+              </Text>
+              <Text
                 style={{
-                  flexDirection: "row",
-                  justifyContent: "space-between",
+                  alignSelf: "flex-end",
+                  fontWeight: "bold",
+                  marginEnd: 4,
                 }}
               >
-                <Text style={{ fontWeight: "bold" }}>Recieved </Text>
-                <Text
-                  style={{
-                    alignSelf: "flex-end",
-                    fontWeight: "bold",
-                    marginEnd: 4,
-                  }}
-                >
-                  <Text style={{ fontSize: 11, color: Colors.gray }}>UGX</Text>{" "}
-                  {amountPaid}
-                </Text>
-              </View>
-              <View
+                {total}
+              </Text>
+            </View>
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "space-between",
+              }}
+            >
+              <Text style={{ fontWeight: "bold" }}>Recieved </Text>
+              <Text
                 style={{
-                  flexDirection: "row",
-                  justifyContent: "space-between",
+                  alignSelf: "flex-end",
+                  fontWeight: "bold",
+                  marginEnd: 4,
                 }}
               >
-                <Text style={{ fontWeight: "bold" }}>Purchased </Text>
-                <Text
-                  style={{
-                    alignSelf: "flex-end",
-                    fontWeight: "bold",
-                    marginEnd: 4,
-                  }}
-                >
-                  <Text style={{ fontSize: 11, color: Colors.gray }}>UGX</Text>
-                  {total}
-                </Text>
-              </View>
-
-              <View
+                {amountPaid}
+              </Text>
+            </View>
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "space-between",
+              }}
+            >
+              <Text style={{ fontWeight: "bold" }}>Purchased </Text>
+              <Text
                 style={{
-                  flexDirection: "row",
-                  justifyContent: "space-between",
+                  alignSelf: "flex-end",
+                  fontWeight: "bold",
+                  marginEnd: 4,
                 }}
               >
-                <Text style={{ fontWeight: "bold" }}>Balance</Text>
-                <Text
-                  style={{
-                    alignSelf: "flex-end",
-                    fontWeight: "bold",
-                    marginEnd: 4,
-                  }}
-                >
-                  <Text style={{ fontSize: 11, color: Colors.gray }}>UGX</Text>{" "}
-                  {balanceGivenOut}
-                </Text>
-              </View>
-            </Table>
+                {total}
+              </Text>
+            </View>
 
             <View
               style={{
                 flexDirection: "row",
                 justifyContent: "space-between",
-                marginTop: 10,
               }}
             >
-              <MaterialButton
-                title="Cancel"
+              <Text style={{ fontWeight: "bold" }}>Balance</Text>
+              <Text
                 style={{
-                  backgroundColor: "transparent",
-                  borderRadius: 5,
-                  borderWidth: 1,
-                  borderColor: Colors.dark,
-                  marginStart: -2,
-                  margin: 10,
-                  height: 40,
-                }}
-                titleStyle={{
+                  alignSelf: "flex-end",
                   fontWeight: "bold",
-                  color: Colors.dark,
+                  marginEnd: 4,
+                  fontSize: 15,
                 }}
-                buttonPress={() => {
-                  setVisible();
-                  resetList()
-                }}
-              />
-              <MaterialButton
-                title="OK"
-                style={{
-                  backgroundColor: Colors.dark,
-                  borderRadius: 5,
-                  borderWidth: 1,
-                  borderColor: Colors.dark,
-                  marginStart: 2,
-                  marginEnd: -2,
-                  margin: 10,
-                  height: 40,
-                }}
-                titleStyle={{
-                  fontWeight: "bold",
-                  color: Colors.primary,
-                }}
-                buttonPress={() => addSale()}
-              />
+              >
+                {balanceGivenOut}
+              </Text>
             </View>
+          </Table>
+
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              marginTop: 10,
+            }}
+          >
+            <MaterialButton
+              title="Cancel"
+              style={{
+                backgroundColor: "transparent",
+                borderRadius: 5,
+                borderWidth: 1,
+                borderColor: Colors.dark,
+                marginStart: -2,
+                margin: 10,
+                height: 40,
+              }}
+              titleStyle={{
+                fontWeight: "bold",
+                color: Colors.dark,
+              }}
+              buttonPress={() => {
+                setVisible();
+                resetList();
+              }}
+            />
+            <MaterialButton
+              title="OK"
+              style={{
+                backgroundColor: Colors.dark,
+                borderRadius: 5,
+                borderWidth: 1,
+                borderColor: Colors.dark,
+                marginStart: 2,
+                marginEnd: -2,
+                margin: 10,
+                height: 40,
+              }}
+              titleStyle={{
+                fontWeight: "bold",
+                color: Colors.primary,
+              }}
+              buttonPress={() => addSale()}
+            />
           </View>
-        </ScrollView>
+        </View>
       </Card>
     </ModalContent>
   );
