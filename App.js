e3bd402e2443263b9cfba52086474cacbfcace70
Ssembatya Moses <104248822+Mosesjespar@users.noreply.github.com>
2023-12-27 16:15:48 +0300

    Update App.js

--- App.js
+++ App.js
@@ -9,30 +9,46 @@ import ShopSummary from "./screens/ShopSummary";
 import Stocking from "./screens/Stocking";
 import { UserSessionUtils } from "./utils/UserSessionUtils";
 import StockPurchaseForm from "./forms/StockPurchaseForm";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import DrawerContent from "./components/DrawerContent";
+
 const Stack = createNativeStackNavigator();
+
+const Drawer = createDrawerNavigator();
+
+const MainStack = () => (
+  <Stack.Navigator
+    screenOptions={{
+      headerShown: false,
+    }}
+  >
+    <Stack.Screen name="login" component={Login} />
+
+    <Stack.Screen name="welcome" component={LandingScreen} />
+
+    <Stack.Screen name="salesEntry" component={SalesEntry} />
+
+    <Stack.Screen name="viewSales" component={ViewSales} />
+
+    <Stack.Screen name="shopSummary" component={ShopSummary} />
+
+    <Stack.Screen name="stocking" component={Stocking} />
+
+    <Stack.Screen name="stockPurchaseForm" component={StockPurchaseForm} />
+  </Stack.Navigator>
+);
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator
+      <Drawer.Navigator
         screenOptions={{
           headerShown: false,
         }}
+        drawerContent={(props) => <DrawerContent {...props} />}
       >
-        <Stack.Screen name="login" component={Login} />
-
-        <Stack.Screen name="welcome" component={LandingScreen} />
-
-        <Stack.Screen name="salesEntry" component={SalesEntry} />
-
-        <Stack.Screen name="viewSales" component={ViewSales} />
-
-        <Stack.Screen name="shopSummary" component={ShopSummary} />
-
-        <Stack.Screen name="stocking" component={Stocking} />
-
-        <Stack.Screen name="stockPurchaseForm" component={StockPurchaseForm} />
-      </Stack.Navigator>
+        <Drawer.Screen name="Main" component={MainStack} />
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 }
