63f39887ac1d5f446a1f175eb5c6db25123fe072
Mosesjespar <mosesjespar@gmail.com>
2024-08-20 12:48:02 +0300

    changes

--- components/EntryBar.js
+++ components/EntryBar.js
@@ -1,55 +1,28 @@
 import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
-import { Image } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import Colors from "@constants/Colors";
-
-const EntryBar = ({ title, target }) => {
+import Icon from "./Icon";
+const EntryBar = ({ title, target, isLast = false, icon = "" }) => {
   const navigation = useNavigation();
   return (
     <TouchableOpacity
       activeOpacity={0.9}
       onPress={() => navigation.navigate(target)}
       style={{
-        backgroundColor: "#303030", //121111
+        backgroundColor: Colors.light_2, //121111
         flexDirection: "row",
-        justifyContent: "space-between",
         alignItems: "center",
+        gap: 20,
+        minHeight: 40,
+        borderBottomColor: "#000",
+        borderBottomWidth: isLast ? 0 : 0.5,
+        padding: 15,
         borderRadius: 5,
-        paddingVertical: 8,
-        paddingHorizontal: 10,
-        marginVertical: 5,
       }}
     >
-      <Text
-        style={{
-          color: Colors.primary,
-        }}
-      >
-        {title}
-      </Text>
-
-      <View
-        style={{
-          borderRadius: 50,
-          backgroundColor: Colors.dark,
-          // padding: 10,
-          height: 25,
-          width: 25,
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <Image
-          source={require("../assets/icons/icons8-right-arrow-50.png")}
-          style={{
-            height: 10,
-            width: 10,
-            resizeMode: "contain",
-            tintColor: Colors.primary,
-          }}
-        />
-      </View>
+      <Icon name={icon} size={18} />
+      <Text style={{}}>{title}</Text>
     </TouchableOpacity>
   );
 };
--- screens/entries/index.js
+++ screens/entries/index.js
@@ -8,17 +8,45 @@ import {
   PDT_ENTRY,
   STOCK_ENTRY_FORM,
 } from "@navigation/ScreenNames";
+import Colors from "@constants/Colors";
 
 const Entries = () => {
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <TopHeader title="Entries" />
 
-      <View style={{ paddingHorizontal: 10, flex: 1, marginTop: 10 }}>
-        <EntryBar title={"Add Stock"} target={STOCK_ENTRY_FORM} />
-        <EntryBar title={"List new product"} target={PDT_ENTRY} />
-        <EntryBar title={"Add Expense"} target={EXPENSE_FORM} />
-        <EntryBar title={"Add Debtor"} target={CLIENT_FORM} />
+      <View
+        style={{
+          paddingHorizontal: 10,
+          flex: 1,
+          marginTop: 10,
+          backgroundColor: Colors.light, //121111
+        }}
+      >
+        <View
+          style={{
+            borderRadius: 5,
+            borderWidth: 0.5,
+          }}
+        >
+          <EntryBar
+            title={"Add Stock"}
+            target={STOCK_ENTRY_FORM}
+            icon="store"
+          />
+          <EntryBar
+            title={"List new product"}
+            target={PDT_ENTRY}
+            icon="cart-plus"
+          />
+          <EntryBar title={"Add Expense"} target={EXPENSE_FORM} icon="wallet" />
+          <EntryBar
+            title={"Add Debtor"}
+            target={CLIENT_FORM}
+            isLast
+            icon="hand-holding"
+          />
+        </View>
       </View>
     </SafeAreaView>
   );
